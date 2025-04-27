9ad933c7112fd6f596c5bdab26f8a13edeacc91d
Ali Toghiani <ali.toghiani.dev@gmail.com>
2025-04-27 12:39:11 +0330

    ref(App.jsx): render array items dynamically

--- src/App.jsx
+++ src/App.jsx
@@ -18,14 +18,7 @@ function App() {
         <section id="core-concepts">
           <h2>Time to get started!</h2>
           <ul>
-            <CoreConcepts
-              title={CORE_CONCEPTS[0].title}
-              description={CORE_CONCEPTS[0].description}
-              image={CORE_CONCEPTS[0].image}
-            />
-            <CoreConcepts {...CORE_CONCEPTS[1]} />
-            <CoreConcepts {...CORE_CONCEPTS[2]} />
-            <CoreConcepts {...CORE_CONCEPTS[3]} />
+            {CORE_CONCEPTS.map( (element) => (<CoreConcepts {...element} key={element.title} />))}
           </ul>
         </section>
         <section id="examples">
