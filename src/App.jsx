4521424bf8d9d7f2fafa390036b7d9f732e14c86
Ali Toghiani <ali.toghiani.dev@gmail.com>
2025-04-27 09:32:25 +0330

    feat(App.jsx): show random descriptions

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,15 @@
 function Header() {
+  const reactDescriptions = ['Fundamental', 'Crucial', 'Core']
+  function getRandomDescription(){
+    const ranIndex = Math.floor(Math.random() * reactDescriptions.length)
+    return reactDescriptions[ranIndex];
+  }
   return (
     <header>
         <img src="src/assets/react-core-concepts.png" alt="Stylized atom" />
         <h1>React Essentials</h1>
         <p>
-          Fundamental React concepts you will need for almost any app you are
+          {getRandomDescription()} React concepts you will need for almost any app you are
           going to build!
         </p>
       </header>
